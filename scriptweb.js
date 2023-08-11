93d109cbab35b6c433234512d077e94d31f5f937
PeacefulBz <6411130049@mut.ac.th>
2023-08-11 21:10:45 +0700

    Update scriptweb.js

--- scriptweb.js
+++ scriptweb.js
@@ -1,13 +1,16 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
     const applicationForm = document.getElementById("applicationForm");
 
-    applicationForm.addEventListener("sumbit", function(event){
+    applicationForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
+        // Get form values
         const fullName = document.getElementById("fullName").value;
         const email = document.getElementById("email").value;
         const resume = document.getElementById("resume").value;
 
+        // You can perform further validation and submission here
+        // For this example, we'll just display an alert
         alert("Application submitted:\n\nName: " + fullName + "\nEmail: " + email);
     });
 });
